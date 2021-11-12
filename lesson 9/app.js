2515f5c2ff9c61bd0ceaba3b95e8b1379b9f3b3f
trung1qaz <trung1qaz2wsx3edc4rfv@gmail.com>
2021-11-12 23:01:28 +0700

    Add new folder lesson9

---
+++ lesson 9/app.js
@@ -0,0 +1,121 @@
+
+let productsListArray = [
+    {
+
+      name: "SƠN LÓT KIỀM NỘI THẤT TIÊU CHUẨN",
+      image:
+        "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/249408448_434852161316548_1306164601287110331_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_ohc=Qi4fK3NDSJ0AX-vLTgJ&_nc_ht=scontent.fhan14-2.fna&oh=d35307a6fb442bd7f824b37a7aea97c7&oe=619FA881",
+      id: "Mã sản phẩm: E600",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+    {
+
+      name: "SƠN LÓT KHÁNG KIỀM NGOẠI THẤT",
+      image:
+        "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/248362096_258986262852234_6295933372306300362_n.png?_nc_cat=100&ccb=1-5&_nc_sid=ae9488&_nc_ohc=Y13vtFdeimAAX_QFdh-&_nc_ht=scontent.fhan14-2.fna&oh=55309e3b543598aa492e9b48eb8ada36&oe=619D4DE3",
+      id: "Mã sản phẩm: E517",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+    {
+
+      name: "SƠN SIÊU BÓNG NỘI THẤT CAO CẤP 18L",
+      image:
+        "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/246664590_607182060317095_2400608385205577477_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_ohc=1USFhrO6ypQAX_mgjQQ&_nc_ht=scontent.fhan14-2.fna&oh=2629891411b9de288825784b1301fe62&oe=61A0D7AC",
+      id: "Mã sản phẩm: E579",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+    {
+
+      name: "SƠN LÓT KHÁNG MUỐI ĐẶC BIỆT",
+      image:
+        "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.15752-9/247474772_207963321340313_1538129219962970124_n.png?_nc_cat=110&ccb=1-5&_nc_sid=ae9488&_nc_ohc=L2xkgiEwgMAAX8_jcLP&_nc_ht=scontent.fhan14-1.fna&oh=4e7ed338afb42e47eea7d269c9bc318d&oe=61A08060",
+      id: "Mã sản phẩm: E525",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+    {
+
+      name: "SƠN CHỐNG THẤM MÀU CAO CẤP",
+      image:
+        "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/246719405_562377358390295_6867033778467818886_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=JpbQUO1G700AX9y9KZY&_nc_ht=scontent.fhan14-2.fna&oh=73979c3e807cacf35adef0ef4c8aff2c&oe=619EE0B4",
+      id: "Mã sản phẩm: E516",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+    
+    {
+
+      name: "SƠN MỊN NỘI THẤT",
+      image:
+        "https://scontent.fhan14-1.fna.fbcdn.net/v/t1.15752-9/245545574_1059686338129468_8260970681246396711_n.png?_nc_cat=102&ccb=1-5&_nc_sid=ae9488&_nc_ohc=N0VyJJMBJEcAX9Wnmbq&_nc_ht=scontent.fhan14-1.fna&oh=b290d5e53d59affe15b4a134cf9a6e0d&oe=619E95C8",
+      id: "Mã sản phẩm: E556",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+    {
+
+      name: "SƠN LÓT KIỀM NỘI THẤT TIÊU CHUẨN",
+      image:
+        "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/249408448_434852161316548_1306164601287110331_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_ohc=Qi4fK3NDSJ0AX-vLTgJ&_nc_ht=scontent.fhan14-2.fna&oh=d35307a6fb442bd7f824b37a7aea97c7&oe=619FA881",
+      id: "Mã sản phẩm: E600",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+
+    {
+
+      name: "SƠN SIÊU BÓNG NỘI THẤT CAO CẤP 18L",
+      image:
+        "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/246664590_607182060317095_2400608385205577477_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_ohc=1USFhrO6ypQAX_mgjQQ&_nc_ht=scontent.fhan14-2.fna&oh=2629891411b9de288825784b1301fe62&oe=61A0D7AC",
+      id: "Mã sản phẩm: E579",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+
+    {
+
+      name: "SƠN CHỐNG THẤM MÀU CAO CẤP",
+      image:
+        "https://scontent.fhan14-2.fna.fbcdn.net/v/t1.15752-9/246719405_562377358390295_6867033778467818886_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=JpbQUO1G700AX9y9KZY&_nc_ht=scontent.fhan14-2.fna&oh=73979c3e807cacf35adef0ef4c8aff2c&oe=619EE0B4",
+      id: "Mã sản phẩm: E516",
+      discount: "",
+      ct: "> Xem chi tiết <",
+    },
+    
+
+
+
+  ];
+
+const productList = document.getElementById("product-list");
+
+function renderProductList() {
+    for (let product of productsListArray){
+        productList.innerHTML += `
+            <div class="product-item">
+                <img
+                    src="${product.image}"
+                    alt=""
+                 />
+                <h3>${product.name}</h3>
+                <p>${product.id}</p>
+                <div class="sale-off">
+                    <p>${product.discount}</p>
+                </div>
+                <div class="call-to-action">
+                    <p></p>
+                </div>
+                <div class="product-item-info">
+                    <p><a href="./detail.html"><h1>${product.ct}<h1></a></p>
+                    
+
+                </div>
+            </div>
+        `;
+    }
+};
+
+renderProductList();
